3eb43d8e4362d3c6efdb6aeb8609a0bb91334259
Maick Souza <maick_a_s@msn.com>
2020-08-14 17:38:18 -0300

    docs(web): eslint

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,14 +4,12 @@ import { Container } from './styles';
 
 import TopImage from '../../assets/TopImage.png';
 
-function Footer () {
-  return (
+const Footer: React.FC = () => (
   <Container>
     <h1>Footer</h1>
 
     <img src={TopImage} alt="Voltar ao topo"/>
   </Container>
-  );
-}
+);
 
 export default Footer;
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 
-import { Container, Content, WelcomeContainer, SkillsContainer } from './styles';
+import {
+  Container, Content, WelcomeContainer, SkillsContainer,
+} from './styles';
 
 import DevImage from '../../assets/DevImage.png';
 import PcImage from '../../assets/PcImage.png';
+
 import Footer from '../../components/Footer';
 
- function Index () {
-   return (
+function Index() {
+  return (
      <Container>
+
       <h1>Header</h1>
         <Content>
               <WelcomeContainer>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. sapien tincidunt, nec commodo lacus vulputate. </p>  
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. sapien tincidunt, nec commodo lacus vulputate. </p>
 
                 <img src={DevImage} alt="Bem-vindo"/>
               </WelcomeContainer>
 
               <SkillsContainer>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. sapien tincidunt, nec commodo lacus vulputate. </p>  
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. sapien tincidunt, nec commodo lacus vulputate. </p>
 
                 <img src={PcImage} alt="Bem-vindo"/>
               </SkillsContainer>
         </Content>
       <Footer />
     </Container>
-   );
- }
+  );
+}
 
- export default Index;
+export default Index;
